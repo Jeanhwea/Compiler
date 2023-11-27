2cbbbd84ffb56dc1402d167d23a9f2ac8943c739
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-27 19:59:01 +0800

    add

--- inc/common.h
+++ inc/common.h
@@ -1,5 +1,6 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
+#include <stdio.h>
 
 char *PL0_VERSION = "v0.10.0";
 
--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include "common.h"
-#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
