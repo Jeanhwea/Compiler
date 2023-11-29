99c1e0124bd103a12c0b22d3d7bb4a5a5688795c
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-11-29 09:19:14 +0800

    add: 添加 enum 类型的定义

--- inc/global.h
+++ inc/global.h
@@ -1,8 +1,16 @@
 #ifndef _GLOBAL_H_
 #define _GLOBAL_H_
 
-enum phase_t { INIT, LEXICAL, SYNTAX, SEMANTIC, IR, CODE_GEN };
+// compiler phase
+typedef enum _phase_enum {
+	/* 0 */ INIT,
+	/* 1 */ LEXICAL,
+	/* 2 */ SYNTAX,
+	/* 3 */ SEMANTIC,
+	/* 4 */ IR,
+	/* 5 */ CODE_GEN
+} phase_e;
 
-extern enum phase_t phase;
+extern phase_e phase;
 
 #endif /* End of _GLOBAL_H_ */
--- src/init.c
+++ src/init.c
@@ -2,4 +2,4 @@
 
 char *PL0_VERSION = "v0.10.0";
 
-enum phase_t phase = INIT;
+phase_e phase = INIT;
