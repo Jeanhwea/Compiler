3d4f2eaefcb42be5c42046edb6ea4f09a126837b
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-07 20:25:46 +0800

    add parser codes

--- src/parse.c
+++ src/parse.c
@@ -3,6 +3,7 @@
 #include "lexical.h"
 #include "scan.h"
 #include "syntax.h"
+#include <stdlib.h>
 
 // current token
 static token_t currtok;
@@ -31,8 +32,10 @@ static pgm_p parse_pgm(void)
 {
 	pgm_p t;
 	INITMEM(pgm, t);
+
 	t->bp = parse_block();
 	match(SS_DOT);
+
 	return t;
 }
 
@@ -44,6 +47,7 @@ static block_p parse_block(void)
 {
 	block_p t;
 	INITMEM(block, t);
+
 	if (CURRTOK_ANY(KW_CONST)) {
 		t->cdp = parse_const_dec();
 	} else if (CURRTOK_ANY(KW_VAR)) {
@@ -55,6 +59,7 @@ static block_p parse_block(void)
 	} else {
 		unlikely();
 	}
+
 	return t;
 }
 
@@ -66,6 +71,7 @@ static const_dec_p parse_const_dec(void)
 {
 	const_dec_p t, p, q;
 	INITMEM(const_dec, t);
+
 	match(KW_CONST);
 	t->cdp = parse_const_def();
 
@@ -75,8 +81,8 @@ static const_dec_p parse_const_dec(void)
 		p->next = q;
 		q->cdp = parse_const_def();
 	}
-
 	match(SS_SEMI);
+
 	return t;
 }
 
@@ -125,45 +131,193 @@ static const_def_p parse_const_def(void)
 	} else {
 		unlikely();
 	}
+
 	return t;
 }
 
+/**
+ * vardec ->
+ *	VAR vardef; { vardef;}
+ */
 static var_dec_p parse_var_dec(void)
 {
-	var_dec_p t;
+	var_dec_p t, p, q;
 	INITMEM(var_dec, t);
+
+	match(KW_VAR);
+	t->vdp = parse_var_def();
+	match(SS_SEMI);
+
+	for (p = t; CURRTOK_ANY(MC_ID); p = q) {
+		INITMEM(var_dec, q);
+		p->next = q;
+		q->vdp = parse_var_def();
+		match(SS_SEMI);
+	}
+
 	return t;
 }
+
+/**
+ * vardef ->
+ *	ident {, ident} : type
+ */
 static var_def_p parse_var_def(void)
 {
-	var_def_p t;
+	var_def_p t, p, q;
 	INITMEM(var_def, t);
+
+	int arrlen = 0;
+	t->idp = parse_ident(READCURR);
+
+	for (p = t; CURRTOK_ANY(SS_COMMA); p = q) {
+		match(SS_COMMA);
+		INITMEM(var_def, q);
+		p->next = q;
+		q->idp = parse_ident(READCURR);
+	}
+
+	match(SS_COLON);
+
+	switch (currtok) {
+	case KW_INTEGER:
+		match(KW_INTEGER);
+		for (p = t; p; p = p->next) {
+			p->idp->type = INT_VAR_IDENT;
+		}
+		break;
+	case KW_CHAR:
+		match(KW_CHAR);
+		for (p = t; p; p = p->next) {
+			p->idp->type = INT_VAR_IDENT;
+		}
+		break;
+	case KW_ARRAY: // array[10] of integer
+		match(KW_ARRAY);
+		match(SS_LBRA);
+		if (CURRTOK_ANY(MC_UNS)) {
+			arrlen = atoi(tokbuf);
+			match(MC_UNS);
+		} else {
+			unlikely();
+		}
+		match(SS_RBBR);
+		match(KW_OF);
+		if (CURRTOK_ANY(KW_INTEGER)) {
+			match(KW_INTEGER);
+			for (p = t; p; p = p->next) {
+				p->idp->type = INTARR_VAR_IDENT;
+				p->idp->length = arrlen;
+			}
+		} else if (CURRTOK_ANY(KW_CHAR)) {
+			match(KW_CHAR);
+			for (p = t; p; p = p->next) {
+				p->idp->type = CHARARR_VAR_IDENT;
+				p->idp->length = arrlen;
+			}
+		} else {
+			unlikely();
+		}
+		break;
+	default:
+		unlikely();
+	}
+
 	return t;
 }
+
+/**
+ * pfdeclist ->
+ *	{ procdec | fundec }
+ */
 static pf_dec_list_p parse_pf_dec_list(void)
 {
-	pf_dec_list_p t;
-	INITMEM(pf_dec_list, t);
+	pf_dec_list_p t, p, q;
+
+	for (p = t = NULL; CURRTOK_ANY2(KW_FUNCTION, KW_PROCEDURE); p = q) {
+		INITMEM(pf_dec_list, q);
+		if (p == NULL) {
+			t = q;
+		} else {
+			p->next = q;
+		}
+		switch (currtok) {
+		case KW_PROCEDURE:
+			q->type = PROC_PFDEC;
+			q->pdp = parse_proc_dec();
+			break;
+		case KW_FUNCTION:
+			q->type = FUN_PFDEC;
+			q->fdp = parse_fun_dec();
+			break;
+		default:
+			unlikely();
+		}
+	}
+
 	return t;
 }
+
+/**
+ * procdec ->
+ *	procdef {; procdef};
+ */
 static proc_dec_p parse_proc_dec(void)
 {
-	proc_dec_p t;
+	proc_dec_p t, p, q;
 	INITMEM(proc_dec, t);
+
+	t->pdp = parse_proc_def();
+	match(SS_SEMI);
+
+	for (p = t; CURRTOK_ANY(KW_PROCEDURE); p = q) {
+		INITMEM(proc_dec, q);
+		p->next = q;
+		q->pdp = parse_proc_def();
+		match(SS_SEMI);
+	}
+
 	return t;
 }
+
+/**
+ * procdef ->
+ *	prochead block
+ */
 static proc_def_p parse_proc_def(void)
 {
 	proc_def_p t;
 	INITMEM(proc_def, t);
+	t->php = parse_proc_head();
+	t->bp = parse_block();
 	return t;
 }
+
+/**
+ * prochead ->
+ *	PROCEDURE ident '(' [paralist] ')' ;
+ */
 static proc_head_p parse_proc_head(void)
 {
 	proc_head_p t;
 	INITMEM(proc_head, t);
+
+	match(KW_PROCEDURE);
+	t->idp = parse_ident(READCURR);
+	t->idp->type = PROC_IDENT;
+
+	match(SS_LPAR);
+	if (CURRTOK_ANY2(KW_VAR, MC_ID)) {
+		t->plp = parse_para_list();
+	} else {
+		unlikely();
+	}
+	match(SS_RPAR);
+	match(SS_SEMI);
+
 	return t;
 }
+
 static fun_dec_p parse_fun_dec(void)
 {
 	fun_dec_p t;
