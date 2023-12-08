5a605768206c60600599a26c78bd0839fc316703
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-08 10:33:05 +0800

    xxx

--- inc/debug.h
+++ inc/debug.h
@@ -4,15 +4,17 @@
 
 void quit(char *file, int line, const char *func, int errno, char *msg);
 
+// print message
 #define msg(fmt, args...) printf(fmt, ##args)
 
+// debug print message
 #define dbg(fmt, args...)                                                      \
 	printf("%s:%d %s(): " fmt, __FILE__, __LINE__, __func__, ##args)
 
 // panic function
 #define panic(msg) quit(__FILE__, __LINE__, __func__, EPANIC, msg);
 
-// debug function
+// debug unlikely case function
 #define unlikely() quit(__FILE__, __LINE__, __func__, EABORT, "unlikely case");
 
 #endif /* End of _DEBUG_H_ */
