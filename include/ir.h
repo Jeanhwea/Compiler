45f60d74c7a01cf463182cc39df80b44c5d76a22
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-13 20:04:02 +0800

    add

---
+++ include/ir.h
@@ -0,0 +1,60 @@
+#ifndef _IR_H_
+#define _IR_H_
+#include "symtab.h"
+
+typedef struct _inst_struct inst_s;
+
+typedef enum {
+	// Arithmetic
+	/*  0 */ ADD_OP,
+	/*  1 */ SUB_OP,
+	/*  2 */ MUL_OP,
+	/*  3 */ DIV_OP,
+	// Unary
+	/*  4 */ INC_OP,
+	/*  5 */ DEC_OP,
+	// Binary
+	/*  6 */ NEG_OP,
+	// Save and Store
+	/*  7 */ LOAD_OP,
+	/*  8 */ ASS_OP,
+	/*  9 */ AARR_OP,
+	// Conditional Branch
+	/* 10 */ EQU_OP,
+	/* 11 */ NEQ_OP,
+	/* 12 */ GTT_OP,
+	/* 13 */ GEQ_OP,
+	/* 14 */ LST_OP,
+	/* 15 */ LEQ_OP,
+	// Unconditional Branch
+	/* 16 */ JMP_OP,
+	// Stack Management
+	/* 17 */ PUSH_OP,
+	/* 18 */ PUSHA_OP,
+	/* 19 */ POP_OP,
+	// Function Management
+	/* 20 */ CALL_OP,
+	/* 21 */ SRET_OP,
+	/* 22 */ ENTER_OP,
+	/* 23 */ FIN_OP,
+	// I/O Management
+	/* 24 */ READ_OP,
+	/* 25 */ READC_OP,
+	/* 26 */ WRS_OP,
+	/* 27 */ WRI_OP,
+	/* 28 */ WRC_OP,
+	// Label Marker
+	/* 29 */ LABEL_OP
+} op_t;
+
+// Instruction struct
+typedef struct _inst_struct {
+	op_t op;
+	sym_entry_s *r;
+	sym_entry_s *s;
+	sym_entry_s *d;
+	inst_s *prev;
+	inst_s *next;
+} inst_s;
+
+#endif /* End of _IR_H_ */
--- include/symtab.h
+++ include/symtab.h
@@ -3,24 +3,58 @@
 
 #define MAXBUCKETS 16
 
-typedef sym_table *sym_table_p;
-typedef sym_bucket *sym_bucket_p;
-typedef sym_entry *sym_entry_p;
+typedef struct _sym_entry_struct sym_entry_s;
+typedef struct _sym_bucket_struct sym_bucket_s;
+typedef struct _sym_table_struct sym_table_s;
 
-struct _sym_entry_struct {
+typedef enum {
+	/*  0 */ NOP_OBJ,
+	/*  1 */ CONST_OBJ,
+	/*  2 */ VAR_OBJ,
+	/*  3 */ PROC_OBJ,
+	/*  4 */ FUN_OBJ,
+	/*  5 */ ARRAY_OBJ,
+	/*  6 */ PARA_VAL_OBJ,
+	/*  7 */ PARA_REF_OBJ,
+	// ADDITIONAL TYPE
+	/*  9 */ TMP_OBJ,
+	/* 10 */ LABEL_OBJ,
+	/* 11 */ NUM_OBJ,
+	/* 12 */ STRING_OBJ
+} obj_t;
+
+typedef enum {
+	/* 0 */ INT_TYPE,
+	/* 1 */ CHAR_TYPE,
+	/* 2 */ NOP_TYPE
+} type_t;
+
+typedef struct _sym_entry_struct {
+	// identifier name
 	char *name;
+	// namespace label
 	char *label;
 	// array length, const value, unsigned value
 	int value;
-} sym_entry;
+	// referenced line number
+	int lineno;
+	// on which level
+	int level;
+	//on which positon
+	int pos;
+	// object type
+	obj_t obj;
+	// type
+	type_t type;
+} sym_entry_s;
 
-struct _sym_bucket_struct {
-	sym_entry_p entry;
-	sym_bucket_p next;
-} sym_bucket;
+typedef struct _sym_bucket_struct {
+	sym_entry_s *entry;
+	sym_bucket_s *next;
+} sym_bucket_s;
 
-struct _sym_table_struct {
-	sym_bucket buckets[MAXBUCKETS];
-} sym_table;
+typedef struct _sym_table_struct {
+	sym_bucket_s buckets[MAXBUCKETS];
+} sym_table_s;
 
 #endif /* End of _SYMTAB_H_ */
