05a2f64a6ff3810db220372e94e87656d62a162a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-14 13:03:13 +0800

    add

--- tests/lex01.c
+++ tests/lex01.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
 	token_t tok;
 	while ((tok = gettok()) != ENDFILE) {
 		msg("line%03d: %s", lineno, linebuf);
-		for (int i = 0; i < colmno + 7; ++i) {
+		for (int i = 0; i < colmno + 8; ++i) {
 			msg(" ");
 		}
-		msg("^ token=%d, buf=%s\n", tok, tokbuf);
+		msg("^ token=%d, buf=[%s]\n", tok, tokbuf);
 	}
 
 	return 0;
