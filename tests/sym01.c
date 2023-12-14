1460bb42e85b242d4b2311b4d4dc11855de3c70f
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-14 09:49:09 +0800

    cleanup codes

--- tests/sym01.c
+++ tests/sym01.c
@@ -9,18 +9,38 @@ void add_test_symbol(char *name)
 	symadd(e);
 }
 
+void func01()
+{
+	syment_t *e;
+	scope_entry("func01");
+	symdump();
+
+	add_test_symbol("aaa");
+	add_test_symbol("ccc");
+	symdump();
+
+	e = symfind("aaa");
+	assert(!strcmp(e->name, "aaa"));
+	assert(!strcmp(e->stab->nspace, "func01"));
+	e = symfind("bbb");
+	assert(!strcmp(e->name, "bbb"));
+	assert(!strcmp(e->stab->nspace, "root"));
+	e = symfind("ccc");
+	assert(!strcmp(e->name, "ccc"));
+	assert(!strcmp(e->stab->nspace, "func01"));
+
+	scope_exit();
+}
+
 int main(int argc, char *argv[])
 {
 	syment_t *e;
-
 	scope_entry("root");
 
 	add_test_symbol("aaa");
-
 	symdump();
 
 	add_test_symbol("bbb");
-
 	symdump();
 
 	e = symfind("aaa");
@@ -30,24 +50,7 @@ int main(int argc, char *argv[])
 	e = symfind("ccc");
 	assert(e == NULL);
 
-	scope_entry("test01");
-
-	add_test_symbol("aaa");
-	add_test_symbol("ccc");
-
-	symdump();
-
-	e = symfind("aaa");
-	assert(!strcmp(e->name, "aaa"));
-	assert(!strcmp(e->stab->nspace, "test01"));
-	e = symfind("bbb");
-	assert(!strcmp(e->name, "bbb"));
-	assert(!strcmp(e->stab->nspace, "root"));
-	e = symfind("ccc");
-	assert(!strcmp(e->name, "ccc"));
-	assert(!strcmp(e->stab->nspace, "test01"));
-
-	scope_exit();
+	func01();
 
 	e = symfind("ccc");
 	assert(e == NULL);
