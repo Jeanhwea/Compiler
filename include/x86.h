9ca383f9c1b8b2a01ba6b2901f7a6dc06e1273d0
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-18 14:42:24 +0800

    add declare for x86

--- include/x86.h
+++ include/x86.h
@@ -13,7 +13,38 @@ typedef struct _reg_struct {
 reg_t *getreg();
 void putreg(reg_t *r);
 
-// asm instructions
 #define ALIGN 4
 #define OFFSET(e) (ALIGN * e->off)
+
+// asm instructions
+void x86_enter(syment_t *e);
+void x86_mov(reg_t *reg, syment_t *var);
+void x86_mov2(syment_t *var, reg_t *reg);
+void x86_mov3(reg_t *reg, syment_t *arr, reg_t *off);
+void x86_mov4(syment_t *arr, reg_t *off, reg_t *reg);
+void x86_mov5(reg_t *r1, reg_t *r2);
+void x86_mov6(reg_t *reg, int num);
+void x86_lea(reg_t *reg, syment_t *var);
+void x86_lea2(reg_t *reg, syment_t *arr, reg_t *off);
+void x86_add(reg_t *r1, reg_t *r2);
+void x86_sub(reg_t *r1, reg_t *r2);
+void x86_mul(reg_t *r1, reg_t *r2);
+void x86_div(reg_t *r1);
+void x86_inc(reg_t *r1);
+void x86_dec(reg_t *r1);
+void x86_pop(reg_t *reg);
+void x86_push(reg_t *reg);
+void x86_push2(syment_t *var);
+void x86_call(syment_t *func);
+void x86_ret();
+void x86_label(syment_t *lab);
+void x86_jmp(syment_t *lab);
+void x86_cmp(reg_t *r1, reg_t *r2);
+void x86_jz(syment_t *lab);
+void x86_jnz(syment_t *lab);
+void x86_jg(syment_t *lab);
+void x86_jng(syment_t *lab);
+void x86_jl(syment_t *lab);
+void x86_jnl(syment_t *lab);
+void x86_sret(syment_t *reg);
 #endif /* End of _X86_H_ */
