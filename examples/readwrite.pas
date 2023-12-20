5a185037c697a5f630f57b4e5d27ad79a744c5ed
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-20 10:41:18 +0800

    add

--- examples/readwrite.pas
+++ examples/readwrite.pas
@@ -2,8 +2,16 @@ var ch: char;
 begin
    read(ch);
    write(ch);
+   ch = 10;
+   write(ch);
+
    read(ch);
    write(ch);
+   ch = 10;
+   write(ch);
+
    read(ch);
+   write(ch);
+   ch = 10;
    write(ch)
 end.
