cdc47e875f9ad1f02dfe01f684159660ba090261
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-20 09:14:15 +0800

    add

--- include/asm.h
+++ include/asm.h
@@ -3,9 +3,11 @@
 #include "ir.h"
 
 #define LIBEXIT "_libexit"
-#define LIBWCHR "_libwrtchar"
-#define LIBWSTR "_libwrtstr"
-#define LIBWINT "_libwrtint"
+#define LIBWCHR "_libwtchar"
+#define LIBWSTR "_libwtstr"
+#define LIBWINT "_libwtint"
+#define LIBRCHR "_librdchar"
+#define LIBRINT "_librdint"
 
 void genasm();
 #endif /* End of _ASM_H_ */
