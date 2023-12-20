470a0e18f0bf20266176c01d8e564d1bea770d81
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-20 14:34:28 +0800

    add

--- examples/readwrite.pas
+++ examples/readwrite.pas
@@ -1,10 +1,8 @@
 var ch: char; i : integer;
 begin
-   for i := 1 to 2 do
-   begin
-      read(ch);
-      write(ch);
-      ch := 10;
-      write(ch)
-   end
-end.
+   read(ch);
+   write(ch);
+   ch := 10;
+   write(ch)
+end
+.
