e83cf7c910ca493695bfd12764ce48450b048914
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-20 08:53:04 +0800

    add sanity check

--- source/post.c
+++ source/post.c
@@ -6,14 +6,16 @@ void post_nasm()
 {
 	char cmd[1024];
 	sprintf(cmd, "nasm -f elf -o %s %s", PL0C_OBJECT, PL0C_ASSEM);
-	// msg("%s\n", cmd);
-	system(cmd);
+	errnum = system(cmd);
+	chkerr("post_nasm fail and exit.");
+	msg("assemble object file %s", PL0C_OBJECT);
 }
 
 void post_link()
 {
 	char cmd[1024];
 	sprintf(cmd, "ld -m elf_i386 -o %s %s", PL0C_TARGET, PL0C_OBJECT);
-	// msg("%s\n", cmd);
-	system(cmd);
+	errnum = system(cmd);
+	chkerr("post_link fail and exit.");
+	msg("link target file %s", PL0C_TARGET);
 }
