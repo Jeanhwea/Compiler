8703948937a123ac474536b6d1b6e4504aa07fb0
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-23 22:42:43 +0800

    add

---
+++ benchmark/t01-writeln.pas
@@ -0,0 +1,14 @@
+const a = 1, b = 2, c = 3;
+procedure writeln(x : integer);
+var newline : char;
+begin
+   write(x);
+   newline = 10;
+   write(newline)
+end;
+
+begin
+   writeln(a);
+   writeln(b);
+   writeln(c)
+end.
--- tool/runtest.c
+++ tool/runtest.c
@@ -1,6 +1,6 @@
+#include "global.h"
 #include "debug.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "glob.h"
 
 #define MAXOUTPUT 4096
 char actual[MAXOUTPUT];
@@ -22,8 +22,22 @@ void runtest()
 	pclose(fp);
 }
 
+void getpas()
+{
+	glob_t glob_result;
+	glob("test/*.pas", GLOB_TILDE, NULL, &glob_result);
+
+	int i;
+	for (i = 0; i < glob_result.gl_pathc; ++i) {
+		printf("%s\n", glob_result.gl_pathv[i]);
+	}
+
+	globfree(&glob_result);
+}
+
 int main(int argc, char *argv[])
 {
-	runtest();
+	// runtest();
+	getpas();
 	return 0;
 }
