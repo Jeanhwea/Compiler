6aa506f4f1d44b810b0fc3dd2610c11f5700efa0
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-25 10:55:25 +0800

    add

---
+++ test/t42-funcall.pas
@@ -0,0 +1,15 @@
+var x, y, z: integer;
+procedure newline(); var nl: char; begin nl := 10; write(nl) end;
+function add(x, y : integer): integer; begin add := x + y; end;
+function sub(x, y : integer): integer; begin sub := x - y; end;
+
+begin
+   x := 1; y := 2;
+   z := add(x,y);
+   write(z); newline();
+   z := sub(x,y);
+   write(z); newline();
+
+   write(add(x,y)); newline();
+   write(sub(x,y)); newline();
+end.
