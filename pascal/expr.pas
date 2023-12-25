78a0c0442c85df29ca128dff77dc0deadeaca4e9
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-25 10:17:30 +0800

    add expr test

--- pascal/expr.pas
+++ pascal/expr.pas
@@ -6,6 +6,10 @@ begin
    x := a + 2 + 3; write(x); newline();
    write(x+a); newline();
    write(a+x); newline();
+   write(1+a+x); newline();
    y := 2 + a - 9; write(y); newline();
    write(3*x); newline();
+   z := a + 2 * 3; write(z); newline();
+   z := 2 * 3 + 4; write(z); newline();
+   write(z*z); newline();
 end.
--- test/t22-expr.pas
+++ test/t22-expr.pas
@@ -3,6 +3,13 @@ var x, y: integer;
 procedure newline(); var nl : char; begin nl := 10; write(nl) end;
 
 begin
-   write(1+2); newline();
-   write(6-2); newline();
+   x := a + 2 + 3; write(x); newline();
+   write(x+a); newline();
+   write(a+x); newline();
+   write(1+a+x); newline();
+   y := 2 + a - 9; write(y); newline();
+   write(3*x); newline();
+   z := a + 2 * 3; write(z); newline();
+   z := 2 * 3 + 4; write(z); newline();
+   write(z*z); newline();
 end.
