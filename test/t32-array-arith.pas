5477ac9b041241786f1d6a5f3207ce631f2c9d8a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-25 10:44:23 +0800

    update

---
+++ test/t32-array-arith.pas
@@ -0,0 +1,17 @@
+var x : integer;  a: array[5] of integer;
+procedure newline(); var nl: char; begin nl := 10; write(nl) end;
+
+begin
+   a[0] := 0;
+   a[1] := 1;
+   a[2] := 2;
+   a[3] := 3;
+   a[4] := 4;
+
+   write(-a[1]); newline();
+   write(-a[1]+a[3]); newline();
+
+   x := -a[2] * a[3];
+   write(x); newline();
+
+end.
