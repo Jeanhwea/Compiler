5a6ba7cc0a61c0b424df1a4ca54ce8f1ed43bd9f
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-26 14:14:33 +0800

    add

--- source/post.c
+++ source/post.c
@@ -7,9 +7,12 @@ void post_nasm()
 {
 	char cmd[1024];
 	sprintf(cmd, "nasm -f elf -o %s %s", PL0E_OBJECT, PL0E_ASSEM);
+	dbg("%s\n", cmd);
+
 	errnum = system(cmd);
 	chkerr("post_nasm fail and exit.");
 	phase = LINK;
+
 	msg("assemble object file %s\n", PL0E_OBJECT);
 }
 
@@ -17,9 +20,12 @@ void post_link()
 {
 	char cmd[1024];
 	sprintf(cmd, "ld -m elf_i386 -o %s %s", PL0E_TARGET, PL0E_OBJECT);
+	dbg("%s\n", cmd);
+
 	errnum = system(cmd);
 	chkerr("post_link fail and exit.");
 	phase = SUCCESS;
+
 	msg("link target file %s\n", PL0E_TARGET);
 }
 
