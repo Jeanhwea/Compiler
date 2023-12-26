5e7a50bde02316caa41f6ca576f8ef9d30fa1c17
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-26 09:33:09 +0800

    add

--- quicktest.pas
+++ quicktest.pas
@@ -1,25 +1,86 @@
-var counter: integer;
+var x, y, z : integer;
 
-function inc(x : integer): integer;
+procedure f11(); var x : integer; begin x := 99 end;
+
+procedure f12(); begin x := 99 end;
+
+procedure f13();
+   procedure f21(); begin y := 99 end;
+   procedure f22(); begin f21() end;
 begin
-   if x < 1 then x := 1;
-   counter := counter + x;
-   inc := counter;
+   f22()
 end;
 
-function id(): integer;
+procedure f14();
+   procedure f21(); var y : integer; begin y := 99 end;
+   procedure f22(); begin f21() end;
 begin
-   id := inc(1)
+   f22()
+end;
+
+procedure f15();
+   procedure f21(); begin y := 99 end;
+   procedure f22(); begin f21() end;
+begin
+   f21()
+end;
+
+procedure f16();
+   procedure f21(); begin z := 88 end;
+   procedure f22();
+      procedure f31(); begin z := 99 end;
+      procedure f32(); begin f21() end;
+   begin f32() end;
+   procedure f23(); begin f22() end;
+begin
+   f23()
+end;
+
+procedure f17();
+   procedure f21(); begin z := 88 end;
+   procedure f22();
+      procedure f31(); begin z := 99 end;
+      procedure f32(); begin f31() end;
+   begin f32() end;
+   procedure f23(); begin f22() end;
+begin
+   f23()
+end;
+
+procedure f18();
+   procedure f21(); begin z := 88 end;
+   procedure f22();
+      procedure f31(); begin z := 99 end;
+      procedure f32(); var z : integer; begin f21() end;
+   begin f32() end;
+   procedure f23(); begin f22() end;
+begin
+   f23()
+end;
+
+procedure f19();
+   procedure f21(); var z : integer; begin z := 88 end;
+   procedure f22();
+      procedure f31(); begin z := 99 end;
+      procedure f32(); begin f21() end;
+   begin f32() end;
+   procedure f23(); begin f22() end;
+begin
+   f23()
 end;
 
 { procedure newline(); begin writeln('') end; }
 procedure newline(); var nl: char; begin nl := 10; write(nl) end;
 
 begin
-   counter := 0;
-   { write(id()); }
-   { write(id()); }
-   write(inc(0)); newline();
-   write(id()); newline();
-   write(inc(3)); newline();
-end.
+   x:=11; y:=22; z:=33; f11(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f12(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f13(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f14(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f15(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f16(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f17(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f18(); write(x); write(y); write(z); newline();
+   x:=11; y:=22; z:=33; f19(); write(x); write(y); write(z); newline();
+end
+.
