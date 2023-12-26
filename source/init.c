36d83a7ee63b8b45ef9596dd6214b033ad84911f
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-12-26 13:31:30 +0800

    v0.16.1

--- source/init.c
+++ source/init.c
@@ -7,7 +7,7 @@
 
 // constants
 char PL0E_NAME[32] = "pc";
-char PL0E_VERSION[32] = "v0.16.0";
+char PL0E_VERSION[32] = "v0.16.1";
 char PL0E_INPUT[4096] = "input.pas";
 char PL0E_ASSEM[4096] = "input.s";
 char PL0E_OBJECT[4096] = "input.o";
