711dcbdefa2c66b468f833c1c78b1821d16ab38c
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-04 15:19:25 +0800

    add example

---
+++ example/syn01.pas
@@ -0,0 +1,10 @@
+var u, v, ans: integer;
+function adder(var x, y : integer):integer;
+begin
+   adder := x + y
+end;
+begin
+   u := 1;
+   v := 2;
+   ans := adder(u, v)
+end.
---
+++ example/syn02.pas
@@ -0,0 +1,5 @@
+var u, v, ans: integer;
+    a : array[10] of integer;
+begin
+   ans := u / a[v] + (u * v) + a[u+v]
+end.
