ab7f4b5d50dbe4b601527841017e44cba84ef6d5
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-07 22:21:31 +0800

    add more test case

--- pascal/run.pas
+++ pascal/run.pas
@@ -1,20 +1,17 @@
-var i: integer;
-
-function fib(x : integer ): integer; begin
-   if x <= 2 then
-      fib := 1
-   else
-      fib := fib(x-1) + fib(x-2);
-end;
+var x: integer;
 
 procedure newline(); begin writeln('') end;
 { procedure newline(); var nl: char; begin nl := 10; write(nl) end; }
 
 begin
-   for i := 1 to 10 do
-   begin
-      write(fib(i));
-      newline();
-   end
+   x := 1;
+   if x > 0 then
+      ;
+   write(x);
+
+   if x > 0 then
+      x := 2;
+
+   write(x)
 end
 .
---
+++ test/t16-ifnull.pas
@@ -0,0 +1,17 @@
+var x: integer;
+
+begin
+   x := 1;
+   if x > 0 then
+      ;
+   write(x);
+
+   if x > 0 then
+      x := 9;
+   write(x);
+
+   if x < 0 then
+      x := 8;
+   write(x)
+end
+.
