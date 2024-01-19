d9eba2d9c733f6576e7e84aba4e70c9526c95de2
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-01-19 17:06:29 +0800

    update

---
+++ nasm/libwchr.s
@@ -0,0 +1,30 @@
+section .text
+	global	_start
+
+_libwchr:
+	push	ebp
+	mov	ebp, esp
+	push	esi
+	push	edi
+	push	ebx
+	mov	[_chrbuf], eax
+	mov	eax, 4
+	mov	ebx, 1
+	mov	ecx, _chrbuf
+	mov	edx, 1
+	int	0x80
+	pop	ebx
+	pop	edi
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+	ret
+
+_start:
+
+	mov	eax, 1
+	xor	ebx, ebx
+	int	0x80
+
+section .data
+	_chrbuf db '?', 0
---
+++ nasm/libwint.s
@@ -0,0 +1,51 @@
+section .text
+	global	_start
+
+_libwint:
+	push	ebp
+	mov	ebp, esp
+	push	esi
+	push	edi
+	push	ebx
+	xor	edi, edi
+	cmp	eax, 0
+	jnl	_noneneg@wint
+	inc	edi
+	neg	eax
+_noneneg@wint:
+	mov	ebx, 10
+	xor	ecx, ecx
+	mov	esi, _intbuf+15
+_loopdigit@wint:
+	xor	edx, edx
+	div	ebx
+	add	edx, '0'
+	mov	byte[esi], dl
+	dec	esi
+	inc	ecx
+	test	eax, eax
+	jnz	_loopdigit@wint
+	test	edi, edi
+	jnz	_negsign@wint
+	inc	esi
+	jmp	_syswrite@wint
+_negsign@wint:
+	mov	byte[esi], '-'
+	inc	ecx
+_syswrite@wint:
+	mov	edx, ecx
+	mov	eax, 4
+	mov	ebx, 1
+	mov	ecx, esi
+	int	0x80
+	pop	ebx
+	pop	edi
+	pop	esi
+	mov	esp, ebp
+	pop	ebp
+	ret
+
+_start:
+
+section .data
+	_intbuf db '????????????????', 0
