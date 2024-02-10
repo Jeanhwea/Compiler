0702903b5a3216d3a1824aa3e6ef1886755a6103
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-02-10 23:01:11 +0800

    add: 添加基本块的定义 bb_t (_basic_block_struct)

--- include/limits.h
+++ include/limits.h
@@ -10,5 +10,7 @@
 #define MAXFIELDLEN 64
 #define MAXDATASEC 128
 #define MAXTEXTSEC 4096
+#define MAXBBLOCK 4096
+#define MAXBBLINK 32
 
 #endif /* End of _LIMITS_H_ */
--- include/optim.h
+++ include/optim.h
@@ -1,4 +1,16 @@
 #ifndef _OPTIM_H_
 #define _OPTIM_H_
+#include "ir.h"
+#include "limits.h"
+
+typedef struct _basic_block_struct bb_t;
+
+typedef struct _basic_block_struct {
+	int bid; // block ID
+	int ninst; // number of instructions
+	inst_t *[MAXBBLOCK] insts; // instructions
+	bb_t *[MAXBBLINK] bb_pre;
+	bb_t *[MAXBBLINK] bb_post;
+} bb_t;
 
 #endif /* End of _OPTIM_H_ */
