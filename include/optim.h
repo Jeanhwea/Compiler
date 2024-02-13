1cabdc2d3f974b04d48674472bea49c72b414ce4
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-02-14 01:37:48 +0800

    update

--- include/optim.h
+++ include/optim.h
@@ -7,19 +7,19 @@ typedef struct _module_struct mod_t;
 typedef struct _function_struct fun_t;
 typedef struct _basic_block_struct bb_t;
 
-typedef struct _module_struct {
+struct _module_struct {
 	fun_t *fhead;
 	fun_t *ftail;
-} mod_t;
+};
 
-typedef struct _function_struct {
+struct _function_struct {
 	symtab_t *scope;
 	bb_t *bhead;
 	bb_t *btail;
 	fun_t *next;
-} fun_t;
+};
 
-typedef struct _basic_block_struct {
+struct _basic_block_struct {
 	// basic information
 	int bid; // block ID
 	int total; // total number of instructions
@@ -29,7 +29,7 @@ typedef struct _basic_block_struct {
 	// predecessors and successors
 	bb_t *pred[MAXBBLINK];
 	bb_t *succ[MAXBBLINK];
-} bb_t;
+};
 
 extern mod_t mod;
 extern int bbcnt;
