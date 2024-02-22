66b0df1f63f0a8128bbf9820454905e94500fff6
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-02-22 16:58:49 +0800

    update: 添加 nodecnt

--- source/optim_dag.c
+++ source/optim_dag.c
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "optim.h"
 
-static int dagndcnt = 0;
+// the DAG nodes counter
+static int nodecnt = 0;
 
 static dag_node_t *create_dag_node(void)
 {
 	dag_node_t *node;
 	INITMEM(dag_node_t, node);
-	node->nid = ++dagndcnt;
+	node->nid = ++nodecnt;
 	return node;
 }
