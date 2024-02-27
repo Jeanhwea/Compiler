3036e37ea2d29915aae0e01e85e8be63d3220e97
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-02-27 10:23:49 +0800

    add: 添加符号的设置

--- source/optim_lva.c
+++ source/optim_lva.c
@@ -1,1 +1,13 @@
 #include "optim.h"
+#include "symtab.h"
+#include "util.h"
+
+void bmset(bits_t bits[], syment_t *e)
+{
+	bset(bits, e->sid);
+}
+
+bool bmget(bits_t bits[], syment_t *e)
+{
+	return bget(bits, e->sid);
+}
